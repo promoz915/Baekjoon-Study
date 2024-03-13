dddacaf9180675374c01384112ec254493616855
Dana <72216721+promoz915@users.noreply.github.com>
2024-03-13 19:52:51 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,13 @@
+n = int(input())
+num = n
+
+for i in range(n):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            continue
+        elif word[j] in word[j+1:]:
+            num -= 1
+            break
+
+print(num)
