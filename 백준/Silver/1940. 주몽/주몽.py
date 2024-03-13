59e64c07d01012a19cb1b620d6c48c3d0821d1b6
Dana <72216721+promoz915@users.noreply.github.com>
2024-03-13 19:51:27 +0900

    [Silver IV] Title: 주몽, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+a = list(map(int, input().split()))
+a.sort()
+count = 0
+i = 0
+j = n-1
+
+while i < j:
+    if a[i] + a[j] < m:
+        i += 1
+    elif a[i] + a[j] > m:
+        j -= 1
+    else:
+        i += 1
+        j -= 1
+        count +=1
+
+print(count)
