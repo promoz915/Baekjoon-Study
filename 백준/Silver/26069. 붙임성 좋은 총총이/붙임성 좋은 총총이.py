160704f15c3d78c16f942ce1508d453125bdf736
Dana <72216721+promoz915@users.noreply.github.com>
2024-03-22 22:20:00 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,12 @@
+# import sys; input = sys.stdin.readline
+
+n = int(input())
+dance = {'ChongChong'}
+
+for i in range(1, n+1):
+    a, b = input().split()
+    if a in dance:
+        dance.add(b)
+    if b in dance:
+        dance.add(a)
+print(len(dance))
