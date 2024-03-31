b8b35bd2f13fe1de0af8cac7bc231db49829ccce
Dana <72216721+promoz915@users.noreply.github.com>
2024-03-31 18:15:39 +0900

    [Silver V] Title: 다리 놓기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,12 +1,18 @@
-def factorial(n):
-    num = 1
-    for i in range(1, n+1):
-        num *= i
-    return num
+import sys; input = sys.stdin.readline
+
+d = [[0 for j in range(31)] for i in range(31)]
+
+for i in range(0, 31):
+    d[i][1] = i
+    d[i][0] = 1
+    d[i][i] = 1
+
+for i in range(2, 31):
+    for j in range(1, i):
+        d[i][j] = d[i-1][j] + d[i-1][j-1]
 
 t = int(input())
 
-for _ in range(t):
+for i in range(0, t):
     n, m = map(int, input().split())
-    result = factorial(m) // (factorial(n) * factorial(m - n))
-    print(result)
+    print(d[m][n])
