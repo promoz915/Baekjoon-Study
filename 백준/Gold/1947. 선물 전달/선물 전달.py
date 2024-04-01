79eb4efe38fcf54001c1fbc8510eb94f86d455bd
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-01 13:06:17 +0900

    [Gold III] Title: 선물 전달, Time: 428 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Gold/1947. 선물 전달/선물 전달.py
@@ -0,0 +1,13 @@
+import sys; input = sys.stdin.readline
+
+n = int(input())
+MOD = 1000000000
+
+d = [0] * 1000001
+d[1] = 0
+d[2] = 1
+
+for i in range(3, n+1):
+    d[i] = (i-1) * (d[i-1] + d[i-2]) % MOD
+
+print(d[n])
