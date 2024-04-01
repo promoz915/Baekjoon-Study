c6b89875d617755ca95e04c10c0653567b2f643c
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-01 16:00:07 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,19 +1,19 @@
 import sys; input = sys.stdin.readline
 
 n = int(input())
-arr = [[0] * 10 for _ in range(n+1)]
+d = [[0 for j in range(11)] for i in range(n+1)]
+MOD = 1000000000
 
-for i in range(10):
-    if i == 0:
-        continue
-    arr[1][i] = 1
+for i in range(1, 10):      # 길이가 1일 때 만드는 높이 h로 끝나는 계단 수의 모든 경우의 수는 1
+    d[1][i] = 1
 
 for i in range(2, n+1):
-    for j in range(10):
-        if j == 0:
-            arr[i][j] = arr[i-1][j+1]
-        elif j == 9:
-            arr[i][j] = arr[i-1][j-1]
-        else:
-            arr[i][j] = arr[i-1][j-1] + arr[i-1][j+1]
-print(sum(arr[-1]) % 1000000000)
+    d[i][0] = d[i-1][1]     # n에서 높이가 0이면 n-1에서는 높이가 1이어야 게단 수가 가능
+    d[i][9] = d[i-1][8]     # n에서 높이가 9이면 n-1에서는 높이가 8이어야 계단 수가 가능
+    for j in range(1, 9):
+        d[i][j] = (d[i-1][j-1] + d[i-1][j+1]) % MOD
+
+ans = 0
+for i in range(10):
+    ans = (ans + d[n][i]) % MOD
+print(ans)
