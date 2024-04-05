d5c911c97faf73cfafad42830ba313ff002baa0e
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-05 15:01:51 +0900

    [Gold II] Title: 선분 교차 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.py
@@ -0,0 +1,33 @@
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+
+def ccw(x1, y1, x2, y2, x3, y3):
+    ans = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
+    if ans > 0:
+        return 1
+    elif ans < 0:
+        return -1
+    return 0
+
+def overlap(x1, y1, x2, y2, x3, y3, x4, y4):
+    if min(x1, x2) <= max(x3, x4) and min(x3, x4) <= max(x1, x2) \
+        and min(y1, y2) <= max(y3, y4) and min(y3, y4) <= max(y1, y2):
+        return True
+    return False
+
+def cross(x1, y1, x2, y2, x3, y3, x4, y4):
+    abc = ccw(x1, y1, x2, y2, x3, y3)
+    abd = ccw(x1, y1, x2, y2, x4, y4)
+    cda = ccw(x3, y3, x4, y4, x1, y1)
+    cdb = ccw(x3, y3, x4, y4, x2, y2)
+    if abc * abd == 0 and cda * cdb == 0:
+        return overlap(x1, y1, x2, y2, x3, y3, x4, y4)
+    elif abc * abd <= 0 and cda * cdb <= 0:
+        return True
+    return False
+
+ans = cross(x1, y1, x2, y2, x3, y3, x4, y4)
+if ans:
+    print(1)
+else:
+    print(0)
