bf80ce02324ddc84cbe59db34add6f60928722ca
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-07 21:39:48 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+def func(a, n):
+    if n == 1:
+        return a % c
+    else:
+        temp = func(a, n//2)
+        if n % 2 == 0:
+            return (temp * temp) % c
+        else:
+            return (temp * temp * a) % c
+print(func(a, b))
