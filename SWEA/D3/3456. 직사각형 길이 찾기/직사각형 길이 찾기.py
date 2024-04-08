3c7387c12fc4bdbcbe0c6e3165819bdb38bfc3ae
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-08 16:39:21 +0900

    [D3] Title: 직사각형 길이 찾기, Time: 264 ms, Memory: 49,752 KB -BaekjoonHub

---
+++ SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.py
@@ -0,0 +1,13 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    data = list(map(int, input().split()))
+    ans = 0
+    if data.count(data[0]) == 3:
+        ans = data[0]
+    else:
+        if data.count(max(data)) == 1:
+            ans = max(data)
+        else:
+            ans = min(data)
+    print(f'#{test_case} {ans}')
