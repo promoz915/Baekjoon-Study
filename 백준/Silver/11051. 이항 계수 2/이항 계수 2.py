bc04e40502e3352543e2e86e64e6a73d48b7fe5a
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-10 00:45:38 +0900

    [Silver II] Title: 이항 계수 2, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,17 +1,7 @@
-import sys; input = sys.stdin.readline
+import math
 
-n, k = map(int,input().split())
-d = [[0 for j in range(n+1)] for i in range(n+1)]
-MOD = 10007
+n, k = map(int, input().split())
+ans = math.comb(n, k)
+ans %= 10007
 
-for i in range(n+1):
-    d[i][0] = 1
-    d[i][i] = 1
-    d[i][1] = i
-
-for i in range(2, n+1):
-    for j in range(1, i):
-        d[i][j] = d[i-1][j] + d[i-1][j-1]
-        d[i][j] = d[i][j] % MOD
-
-print(d[n][k])
+print(ans)
