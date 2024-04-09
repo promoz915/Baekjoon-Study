1f302fcaddd63914b741e75248801450567d2881
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-09 20:31:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 6328 ms, Memory: 282332 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if data[i-1][0] <= j:
+            dp[i][j] = max(dp[i-1][j-data[i-1][0]] + data[i-1][1], dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[n][k])
