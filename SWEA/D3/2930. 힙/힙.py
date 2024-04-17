463dc146ffca402be324df7bcb6583a80fa767b5
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-17 23:57:47 +0900

    [D3] Title: 힙, Time: 1,368 ms, Memory: 80,832 KB -BaekjoonHub

--- SWEA/D3/2930. 힙/힙.py
+++ SWEA/D3/2930. 힙/힙.py
@@ -1,40 +1,17 @@
-def swap(a, b):
-    heap[a], heap[b] = heap[b], heap[a]
+import heapq
 
 t = int(input())
-heap = [0] * 100001
-
 for test_case in range(1, t+1):
     n = int(input())
+    heap = []
     ans = []
-    size = 0
     for _ in range(n):
-        order = list(map(int, input().split()))
-
-        if order[0] == 1:
-            data = order[1]
-            temp = size = size + 1
-            heap[temp] = data
-            while temp > 1 and heap[temp] > heap[temp // 2]:
-                swap(temp, temp // 2)
-                temp = temp // 2
-
-        elif order[0] == 2:
-            temp = 1
-            if size == 0:
-                ans.append(-1)
+        data = list(map(int, input().split()))
+        if data[0] == 1:
+            heapq.heappush(heap, -data[1])
+        else:
+            if len(heap) > 0:
+                ans.append(-heapq.heappop(heap))
             else:
-                ans.append(heap[temp])
-                heap[temp] = heap[size]
-                heap[size] = 0
-                size = size - 1
-                while True:
-                    child = temp * 2
-                    if child + 1 <= size and heap[child] < heap[child + 1]:
-                        child += 1
-                    if child > size or heap[child] < heap[temp]:
-                        break
-                    swap(temp, child)
-                    temp = child
-
+                ans.append(-1)
     print(f'#{test_case}', *ans)
