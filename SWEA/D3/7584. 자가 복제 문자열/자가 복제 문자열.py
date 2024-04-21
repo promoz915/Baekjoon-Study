e059607d1eda8ca0d4320b05eb63ee7200d22b7b
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-22 01:25:52 +0900

    [D3] Title: 자가 복제 문자열, Time: 100 ms, Memory: 45,228 KB -BaekjoonHub

--- SWEA/D3/7584. 자가 복제 문자열/자가 복제 문자열.py
+++ SWEA/D3/7584. 자가 복제 문자열/자가 복제 문자열.py
@@ -1,16 +1,14 @@
-T = int(input())
-
-for test_case in range(1, T + 1):
-    k = int(input())
-    temp = 2
-    cnt = 0
-    while k > 2:
-        temp = 2
-        while temp < k:
-            temp *= 2
-        if temp == k:
+t = int(input())
+for test_case in range(1, t+1):
+    k = int(input()) - 1
+    ans = 0
+    while k > 0:
+        if k % 2 == 1:
+            k = (k-1)//2
+        if k % 4 == 0:
+            ans = 0
             break
-        k = temp - k
-        cnt += 1
-    ans = cnt % 2
+        elif k % 2 == 0:
+            ans = 1
+            break
     print(f'#{test_case} {ans}')
