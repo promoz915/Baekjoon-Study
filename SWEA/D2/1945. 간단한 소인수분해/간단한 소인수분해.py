190b92a6901f6799e00fad700bfd1ee1c9de5b20
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-26 16:44:53 +0900

    [D2] Title: 간단한 소인수분해, Time: 100 ms, Memory: 44,728 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,13 +1,16 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
+t = int(input())
+result = []
+
+for test_case in range(1, t+1):
     n = int(input())
     ans = [0] * 5
-    num = [2, 3, 5, 7, 11]
-    
+    check = [2, 3, 5, 7, 11]
+
     for i in range(4, -1, -1):
-        while n % num[i] == 0:
+        while n % check[i] == 0:
             ans[i] += 1
-            n //= num[i]
-    print(f'#{test_case}', end=' ')
-    print(*ans)
+            n //= check[i]
+    result.append(f'#{test_case} {" ".join(map(str, ans))}')
+
+for r in result:
+    print(r)
