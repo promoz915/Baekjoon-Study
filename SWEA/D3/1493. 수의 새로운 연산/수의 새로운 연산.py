58690ca1e11b5e3ea68c58826849bcf03c5ba870
Dana <72216721+promoz915@users.noreply.github.com>
2024-04-29 18:52:19 +0900

    [D3] Title: 수의 새로운 연산, Time: 289 ms, Memory: 62,552 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
@@ -6,9 +6,9 @@ i, j = 1, 1
 for n in range(1, 50000):
     dct[n] = (i,j)
     r_dct[(i,j)] = n
-    i, j = i-1, j+1
-    if i < 1:
-        i, j = j, 1
+    i, j = i+1, j-1
+    if j < 1:
+        i, j = 1, i
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
