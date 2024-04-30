5ad32c8997052f303df8302e530ce12e57de262c
Dana <72216721+promoz915@users.noreply.github.com>
2024-05-01 01:05:41 +0900

    [level 1] Title: 소수 찾기, Time: 169.17 ms, Memory: 24 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    a = [False, False] + [True] * (n-1)
+    prime = []
+    
+    for i in range(2, n+1):
+        if a[i]:
+            prime.append(i)
+            for j in range(i+i, n+1, i):
+                a[j] = False
+    return len(prime)
